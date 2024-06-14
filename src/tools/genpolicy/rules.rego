kata-containers/kata-containers

commit 535cf04edb749052696c6409f62f1a40c92742a8

genpolicy: add shareProcessNamespace support
Validate the sandbox_pidns field value for CreateSandbox and
CreateContainer.

Fixes: #8868

Signed-off-by: Dan Mihai <dmihai@microsoft.com>

src/tools/genpolicy/rules.rego
@@ -45,8 +45,12 @@ CreateContainerRequest {
     some p_container in policy_data.containers
     print("======== CreateContainerRequest: trying next policy container")
 
+    p_pidns := p_container.sandbox_pidns
+    i_pidns := input.sandbox_pidns
+    print("CreateContainerRequest: p_pidns =", p_pidns, "i_pidns =", i_pidns)
+    p_pidns == i_pidns
+
     p_oci := p_container.OCI
-    p_storages := p_container.storages
 
     print("CreateContainerRequest: p Version =", p_oci.Version, "i Version =", i_oci.Version)
     p_oci.Version == i_oci.Version
@@ -55,7 +59,10 @@ CreateContainerRequest {
     p_oci.Root.Readonly == i_oci.Root.Readonly
 
     allow_anno(p_oci, i_oci)
+
+    p_storages := p_container.storages
     allow_by_anno(p_oci, i_oci, p_storages, i_storages)
+
     allow_linux(p_oci, i_oci)
 
     print("CreateContainerRequest: true")
@@ -1074,6 +1081,12 @@ CopyFileRequest {
     regex.match(regex2, input.path)
 
     print("CopyFileRequest: true")
+}
+
+CreateSandboxRequest {
+    i_pidns := input.sandbox_pidns
+    print("CreateSandboxRequest: i_pidns =", i_pidns)
+    i_pidns == false
 }
 
 ExecProcessRequest {

src/tools/genpolicy/src/config_map.rs
@@ -125,4 +125,8 @@ impl yaml::K8sResource for ConfigMap {
     fn use_host_network(&self) -> bool {
         panic!("Unsupported");
     }
+
+    fn use_sandbox_pidns(&self) -> bool {
+        panic!("Unsupported");
+    }
 }

src/tools/genpolicy/src/daemon_set.rs
@@ -129,4 +129,11 @@ impl yaml::K8sResource for DaemonSet {
         }
         false
     }
+
+    fn use_sandbox_pidns(&self) -> bool {
+        if let Some(shared) = self.spec.template.spec.shareProcessNamespace {
+            return shared;
+        }
+        false
+    }
 }

src/tools/genpolicy/src/deployment.rs
@@ -127,4 +127,11 @@ impl yaml::K8sResource for Deployment {
         }
         false
     }
+
+    fn use_sandbox_pidns(&self) -> bool {
+        if let Some(shared) = self.spec.template.spec.shareProcessNamespace {
+            return shared;
+        }
+        false
+    }
 }

src/tools/genpolicy/src/job.rs
@@ -101,4 +101,11 @@ impl yaml::K8sResource for Job {
         }
         false
     }
+
+    fn use_sandbox_pidns(&self) -> bool {
+        if let Some(shared) = self.spec.template.spec.shareProcessNamespace {
+            return shared;
+        }
+        false
+    }
 }

src/tools/genpolicy/src/list.rs
@@ -100,4 +100,8 @@ impl yaml::K8sResource for List {
     fn use_host_network(&self) -> bool {
         panic!("Unsupported");
     }
+
+    fn use_sandbox_pidns(&self) -> bool {
+        panic!("Unsupported");
+    }
 }

src/tools/genpolicy/src/no_policy.rs
@@ -67,4 +67,8 @@ impl yaml::K8sResource for NoPolicyResource {
     fn use_host_network(&self) -> bool {
         panic!("Unsupported");
     }
+
+    fn use_sandbox_pidns(&self) -> bool {
+        panic!("Unsupported");
+    }
 }

src/tools/genpolicy/src/pod.rs
@@ -78,6 +78,9 @@ pub struct PodSpec {
     pub hostNetwork: Option<bool>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
+    pub shareProcessNamespace: Option<bool>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     dnsConfig: Option<PodDNSConfig>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -734,6 +737,13 @@ impl yaml::K8sResource for Pod {
     fn use_host_network(&self) -> bool {
         if let Some(host_network) = self.spec.hostNetwork {
             return host_network;
+        }
+        false
+    }
+
+    fn use_sandbox_pidns(&self) -> bool {
+        if let Some(shared) = self.spec.shareProcessNamespace {
+            return shared;
         }
         false
     }

DevSauce-Official/Ias-Sauce

commit 5075596f4a876cfdcaa92eef027bcd83e36389ba

genpolicy: set CreateSandboxRequest default = false
If the default is true, CreateSandboxRequest always gets evaluated to
true, regardless of the evaluation of the CreateSandboxRequest rules.

Commit 535cf04edb749052696c6409f62f1a40c92742a8 accidentally didn't
include this default value change.

Signed-off-by: Dan Mihai <dmihai@microsoft.com>

src/tools/genpolicy/rules.rego
@@ -15,7 +15,7 @@ default AddSwapRequest := false
 default CloseStdinRequest := false
 default CopyFileRequest := false
 default CreateContainerRequest := false
-default CreateSandboxRequest := true
+default CreateSandboxRequest := false
 default DestroySandboxRequest := true
 default ExecProcessRequest := false
 default GetOOMEventRequest := true